98b7fe11332df2335c411fb8064705d77a0d3d8c
km1t4h <km1t4h@gmail.com>
2026-02-01 19:42:08 +0100

    feat: Refactors mrgimg.sh script. Removes the first part, now it uses t48_first_dump binary uimages

--- scripts/mrgimg.sh
+++ scripts/mrgimg.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-if [ $# -ne 4 ]; then
-    echo "Usage mrgimg.sh <first-part> <size> <second-part> <output-img>"
+if [ $# -ne 3 ]; then
+    echo "Usage mrgimg.sh <part-to-merge> <size> <output-img>"
     exit
 fi
 
+cp t48_first_dump.bin cpy
+
 echo "Start merging image process..."
 echo -ne "                          (0%)\r"
-dd if=${1} of=${4} bs=1 count=${2} status=none
+dd if=cpy of=${3} bs=1 count=${2} status=none
 echo -ne "##########                (50%)\r"
-dd if=${3} of=${4} bs=1 seek=${2} status=none
+dd if=${1} of=${3} bs=1 seek=${2} status=none
 echo -ne "####################      (100%)"
 echo "    Done"
+
+rm -rf cpy
