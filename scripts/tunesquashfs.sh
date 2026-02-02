444f9e97be075a6f9c9e3da3f47367f6221197da
km1t4h <km1t4h@gmail.com>
2026-02-02 19:31:04 +0100

    feat: Add tunesquashfs.sh bash script which adjusts squashfs size

--- scripts/tunesquashfs.sh
+++ scripts/tunesquashfs.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
+if [ $# -ne 4 ]; then
+    echo "Usage tunesquashfs.sh <squashfs-root-dir> <block-size> <compression> <expected-size> <squashfs-output>"
+    exit
+fi
+
 while true; do
-    rm -rf squashfs0_test7
-    mksquashfs squashfs-root/ squashfs0_test7 -comp xz -b 65536
+    rm -rf ${5} 2> /dev/null
+    mksquashfs ${1} ${5} -comp ${3} -b ${2}
 
     random_num=$(tr -dc 0-9 < /dev/urandom | head -c 1; echo)
     random=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 13; echo)
-    size=$(unsquashfs -s squashfs0_test7 | grep "Filesystem size" | grep -oE "[0-9]+{6}")
+    size=$(unsquashfs -s ${5} | grep "Filesystem size" | grep -oE "[0-9]+{6}")
     temp=2
 
-    if [ ${size} -eq 3225548 ]; then
+    if [ ${size} -eq ${4} ]; then
         echo "Equal"
         exit
-    elif [ ${size} -lt 3225548 ]; then
+    elif [ ${size} -lt ${4} ]; then
         echo "Add"
         echo ${random} >> squashfs-root/lib/audio/Red_Alert.g711
-    elif [ ${size} -gt 3225548 ]; then
+    elif [ ${size} -gt ${4} ]; then
         echo "Remove"
         head -c -1 squashfs-root/lib/audio/Red_Alert.g711 > 2
         cat 2 > squashfs-root/lib/audio/Red_Alert.g711
