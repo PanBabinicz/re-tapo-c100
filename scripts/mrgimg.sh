6ea00df187df12b8348e85d5cf47e01c36a539ca
km1t4h <km1t4h@gmail.com>
2026-02-08 19:05:49 +0100

    feat: Modify mrgimg.sh script, it takes the original image as an argument

--- scripts/mrgimg.sh
+++ scripts/mrgimg.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo "Usage mrgimg.sh <part-to-merge> <size> <output-img>"
+if [ $# -ne 4 ]; then
+    echo "Usage mrgimg.sh <part-to-merge> <original-img> <size> <output-img>"
     exit
 fi
 
-cp t48_first_dump.bin cpy
+cp ${2} cpy
 
 echo "Start merging image process..."
 echo -ne "                          (0%)\r"
-dd if=cpy of=${3} bs=1 count=${2} status=none
+dd if=cpy of=${4} bs=1 count=${3} status=none
 echo -ne "##########                (50%)\r"
-dd if=${1} of=${3} bs=1 seek=${2} status=none
+dd if=${1} of=${4} bs=1 seek=${3} status=none
 echo -ne "####################      (100%)"
 echo "    Done"
 
